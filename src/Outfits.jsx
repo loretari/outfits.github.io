066e6a1f0760f50a2958aebb58d59ce19c55854f
loretari <loretari@gmail.com>
2022-10-24 13:38:39 +0300

    init: change style in outfits.jsx

--- src/Outfits.jsx
+++ src/Outfits.jsx
@@ -10,13 +10,13 @@ const Outfits = () => {
     return (
         <main className= "container">
             <div className= "row justify-content-center mt-3 mb-3">
-                <div className= "col-6">
+                <div className= "col-10">
 <Days />
                 </div>
             </div>
             <hr />
             <div className= "row justify-content-center mt-3 mb-3">
-                <div className= "col-10">
+                <div className= "col-12">
                     <div className="card-collection">
 
 <OutfitsOption />
